ecc4da69692dc91574c1781f420433a42c11d5ec
Diegomca98 <diegomca98@gmail.com>
2021-03-23 14:23:57 -0600

    Error detail: JSON parse error - Expecting value: line 1 column 1 (char 0)

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
@@ -7,6 +9,8 @@ class HelloApiView(APIView):
     
     def get(self, request, format=None):
         """Returns a list of APIView features"""
+        serializer_class = serializers.HelloSerializer
+
         an_apiview = [
             'Uses HTTP methods as function(get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
@@ -18,3 +22,20 @@ class HelloApiView(APIView):
             'message': 'Hello!',
             'an_apiview' : an_apiview
         })
+
+    def post(self, request):
+        """Create a hello message with out name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({
+                'message': message,
+            })
+
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
